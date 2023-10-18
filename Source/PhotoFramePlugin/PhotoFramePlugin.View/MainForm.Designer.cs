c295fdb6786193a82d1e10919e395acb68327fb0
Anton Fedorov <63503870+antonfedorov1@users.noreply.github.com>
2023-10-18 15:49:37 +0700

    feat(Source): Можно построить модель фоторамки.

--- Source/PhotoFramePlugin/PhotoFramePlugin.View/MainForm.Designer.cs
+++ Source/PhotoFramePlugin/PhotoFramePlugin.View/MainForm.Designer.cs
@@ -28,34 +28,305 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.OptionsPanel = new System.Windows.Forms.Panel();
+            this.ParameterLimitations = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ParameterValues = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NameOfParameters = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BuildFigure = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.OptionsPanel.SuspendLayout();
+            this.ParameterLimitations.SuspendLayout();
+            this.ParameterValues.SuspendLayout();
+            this.NameOfParameters.SuspendLayout();
+            this.MainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // OptionsPanel
             // 
-            this.button1.Location = new System.Drawing.Point(389, 292);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.OptionsPanel.Controls.Add(this.ParameterLimitations);
+            this.OptionsPanel.Controls.Add(this.ParameterValues);
+            this.OptionsPanel.Controls.Add(this.NameOfParameters);
+            this.OptionsPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.OptionsPanel.Location = new System.Drawing.Point(0, 0);
+            this.OptionsPanel.Name = "OptionsPanel";
+            this.OptionsPanel.Size = new System.Drawing.Size(458, 144);
+            this.OptionsPanel.TabIndex = 3;
+            // 
+            // ParameterLimitations
+            // 
+            this.ParameterLimitations.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ParameterLimitations.Controls.Add(this.label10);
+            this.ParameterLimitations.Controls.Add(this.label8);
+            this.ParameterLimitations.Controls.Add(this.label6);
+            this.ParameterLimitations.Controls.Add(this.label4);
+            this.ParameterLimitations.Controls.Add(this.label2);
+            this.ParameterLimitations.Location = new System.Drawing.Point(309, 12);
+            this.ParameterLimitations.Name = "ParameterLimitations";
+            this.ParameterLimitations.Size = new System.Drawing.Size(137, 132);
+            this.ParameterLimitations.TabIndex = 2;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.label10.Location = new System.Drawing.Point(3, 105);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 19);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "10-30 мм";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.label8.Location = new System.Drawing.Point(3, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 19);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "100-1210 мм";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.label6.Location = new System.Drawing.Point(3, 54);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 19);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "100-1210 мм";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.label4.Location = new System.Drawing.Point(3, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "100-1200 мм";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.label2.Location = new System.Drawing.Point(3, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "100-1200 мм";
+            // 
+            // ParameterValues
+            // 
+            this.ParameterValues.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.ParameterValues.Controls.Add(this.textBox5);
+            this.ParameterValues.Controls.Add(this.textBox4);
+            this.ParameterValues.Controls.Add(this.textBox3);
+            this.ParameterValues.Controls.Add(this.textBox2);
+            this.ParameterValues.Controls.Add(this.textBox1);
+            this.ParameterValues.Location = new System.Drawing.Point(214, 12);
+            this.ParameterValues.Name = "ParameterValues";
+            this.ParameterValues.Size = new System.Drawing.Size(89, 132);
+            this.ParameterValues.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.White;
+            this.textBox5.Location = new System.Drawing.Point(3, 106);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(83, 20);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "100";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.Location = new System.Drawing.Point(3, 80);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(83, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "99";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(3, 54);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(83, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "12";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(3, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(83, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1300";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "90";
+            // 
+            // NameOfParameters
+            // 
+            this.NameOfParameters.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.NameOfParameters.Controls.Add(this.label9);
+            this.NameOfParameters.Controls.Add(this.label7);
+            this.NameOfParameters.Controls.Add(this.label5);
+            this.NameOfParameters.Controls.Add(this.label3);
+            this.NameOfParameters.Controls.Add(this.label1);
+            this.NameOfParameters.Location = new System.Drawing.Point(12, 12);
+            this.NameOfParameters.Name = "NameOfParameters";
+            this.NameOfParameters.Size = new System.Drawing.Size(196, 132);
+            this.NameOfParameters.TabIndex = 0;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(3, 105);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(114, 19);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Толщина рамки";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(3, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(168, 19);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Высота внешней рамки";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(3, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(175, 19);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Ширина внешней рамки";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Высота внутри рамки";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ширина внутри рамки";
+            // 
+            // BuildFigure
+            // 
+            this.BuildFigure.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.BuildFigure.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BuildFigure.Location = new System.Drawing.Point(187, 152);
+            this.BuildFigure.Name = "BuildFigure";
+            this.BuildFigure.Size = new System.Drawing.Size(113, 30);
+            this.BuildFigure.TabIndex = 0;
+            this.BuildFigure.Text = "Построить";
+            this.BuildFigure.UseVisualStyleBackColor = true;
+            this.BuildFigure.Click += new System.EventHandler(this.BuildFigureClick);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.BuildFigure);
+            this.MainPanel.Controls.Add(this.OptionsPanel);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(458, 194);
+            this.MainPanel.TabIndex = 1;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(458, 194);
+            this.Controls.Add(this.MainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(474, 233);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(474, 233);
             this.Name = "MainForm";
-            this.Text = "PhotoFramePlugin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Photo Frame Plagin";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.OptionsPanel.ResumeLayout(false);
+            this.ParameterLimitations.ResumeLayout(false);
+            this.ParameterLimitations.PerformLayout();
+            this.ParameterValues.ResumeLayout(false);
+            this.ParameterValues.PerformLayout();
+            this.NameOfParameters.ResumeLayout(false);
+            this.NameOfParameters.PerformLayout();
+            this.MainPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel OptionsPanel;
+        private System.Windows.Forms.Panel ParameterLimitations;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel ParameterValues;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel NameOfParameters;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BuildFigure;
+        private System.Windows.Forms.Panel MainPanel;
     }
 }
 
--- Source/PhotoFramePlugin/PhotoFramePlugin.View/MainForm.cs
+++ Source/PhotoFramePlugin/PhotoFramePlugin.View/MainForm.cs
@@ -1,48 +1,125 @@
 ﻿using System;
-using Microsoft.Win32;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
-using Kompas6Constants;
-using Kompas6Constants3D;
-using KAPITypes;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
 using Kompas6API5;
-
+using Kompas6Constants3D;
 
 namespace PhotoFramePlugin.View
 {
     public partial class MainForm : Form
     {
-        private KompasObject _kompas;
+        private KompasObject kompas;
 
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void BuildFigureClick(object sender, EventArgs e)
         {
-            if (_kompas == null)
+            if (kompas == null)
             {
                 Type t = Type.GetTypeFromProgID("KOMPAS.Application.5");
-                _kompas = (KompasObject) Activator.CreateInstance(t);
+                kompas = (KompasObject)Activator.CreateInstance(t);
             }
 
-            if (_kompas != null)
+            if (kompas != null)
             {
-                _kompas.Visible = true;
-                _kompas.ActivateControllerAPI();
+                kompas.Visible = true;
+                kompas.ActivateControllerAPI();
             }
 
-            if (_kompas != null)
-            {
-                ksDocument3D iDocument3D = (ksDocument3D) _kompas.Document3D();
-                if (iDocument3D.Create(false /*видимый*/, true /*деталь*/))
-                {
-                    ksPart iPart = (ksPart) iDocument3D.GetPart((short) Part_Type.pNew_Part);
-                }
-            }
+            ksDocument3D iDocument3D = (ksDocument3D)kompas.Document3D();
+            iDocument3D.Create(false /*видимый*/, true /*деталь*/);
+
+            ksPart iPart = iDocument3D.GetPart((short)Part_Type.pTop_Part);
+            //iDocument3D.fileName = "PhotoFrame";
+
+            ksEntity iSketch = iPart.NewEntity((short)Obj3dType.o3d_sketch);
+            ksSketchDefinition iDefinitionSketch = iSketch.GetDefinition();
+            iDefinitionSketch.SetPlane(iPart.GetDefaultEntity((short)Obj3dType.o3d_planeXOY));
+            iSketch.Create();
+
+            double innerX = 300;
+            double innerY = 400;
+
+            double centreInnerX = (innerX / 2) * -1;
+            double centreInnerY = (innerY / 2) * -1;
+            ksRectangleParam innerFrame = (ksRectangleParam)kompas.GetParamStruct(91);
+            innerFrame.x = centreInnerX;
+            innerFrame.y = centreInnerY;
+            innerFrame.height = 400;
+            innerFrame.width = 300;
+            innerFrame.style = 1;
+
+            double otherX = 400;
+            double otherY = 500;
+
+            double centreOtherX = (otherX / 2) * -1;
+            double centreOtherY = (otherY / 2) * -1;
+            ksRectangleParam otherFrame = (ksRectangleParam)kompas.GetParamStruct(91);
+            otherFrame.x = centreOtherX;
+            otherFrame.y = centreOtherY;
+            otherFrame.height = 500;
+            otherFrame.width = 400;
+            otherFrame.style = 1;
+
+            ksDocument2D iDocument2D = iDefinitionSketch.BeginEdit();
+            iDocument2D.ksRectangle(innerFrame, 0);
+            iDocument2D.ksRectangle(otherFrame, 0);
+            iDefinitionSketch.EndEdit();
+
+            // ВЫДАВЛИВАНИЕ
+            ksEntity entityExtr = (ksEntity)iPart.NewEntity((short)Obj3dType.o3d_bossExtrusion);
+            // интерфейс свойств базовой операции выдавливания
+            ksBossExtrusionDefinition extrusionDef = (ksBossExtrusionDefinition)entityExtr.GetDefinition(); // интерфейс базовой операции выдавливания
+            ksExtrusionParam extrProp = (ksExtrusionParam)extrusionDef.ExtrusionParam(); // интерфейс структуры параметров выдавливания
+            extrusionDef.SetSketch(iSketch); // эскиз операции выдавливания
+            extrProp.direction = (short)Direction_Type.dtNormal;      // направление выдавливания (прямое)
+            extrProp.typeNormal = (short)End_Type.etBlind;      // тип выдавливания (строго на глубину)
+            extrProp.depthNormal = 20;         // глубина выдавливания
+            entityExtr.Create();                // создадим операцию
+
+            // задняя подложка рамки
+            ksEntity entitySketch2 = iPart.NewEntity((short)Obj3dType.o3d_sketch);
+            ksSketchDefinition sketchDef2 = entitySketch2.GetDefinition();
+            sketchDef2.SetPlane(iPart.GetDefaultEntity((short)Obj3dType.o3d_planeXOY));  // установим плоскость
+            entitySketch2.Create(); // создадим эскиз
+
+            double innerX2 = 300;
+            double innerY2 = 400;
+
+            double centreInnerX2 = (innerX2 / 2) * -1;
+            double centreInnerY2 = (innerY2 / 2) * -1;
+            ksRectangleParam innerFrame2 = (ksRectangleParam)kompas.GetParamStruct(91);
+            innerFrame2.x = centreInnerX;
+            innerFrame2.y = centreInnerY;
+            innerFrame2.height = 400;
+            innerFrame2.width = 300;
+            innerFrame2.style = 1;
+
+            ksDocument2D sketchEdit2 = sketchDef2.BeginEdit();
+            sketchEdit2.ksRectangle(innerFrame2, 0);
+            sketchDef2.EndEdit();   // завершение редактирования эскиза
+
+            // приклеим выдавливанием
+            ksEntity entityBossExtr = (ksEntity)iPart.NewEntity((short)Obj3dType.o3d_bossExtrusion);
+            ksBossExtrusionDefinition bossExtrDef = (ksBossExtrusionDefinition)entityBossExtr.GetDefinition();
+            ksExtrusionParam extrProp2 = (ksExtrusionParam)bossExtrDef.ExtrusionParam(); // интерфейс структуры параметров выдавливания
+            bossExtrDef.SetSketch(entitySketch2); // эскиз операции выдавливания
+            extrProp2.direction = (short)Direction_Type.dtNormal;      // направление выдавливания (обратное)
+            extrProp2.typeNormal = (short)End_Type.etBlind;      // тип выдавливания (строго на глубину)
+            extrProp2.depthReverse = 2;         // глубина выдавливания
+            entityBossExtr.Create();                // создадим операцию
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
         }
     }
 }
